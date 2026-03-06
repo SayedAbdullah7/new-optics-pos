e30e9e31062643100960d060008f30e5c1ea01d6
SayedAbdullah7 <96739089+SayedAbdullah7@users.noreply.github.com>
2026-03-07 01:10:58 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,10 +22,10 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // User::factory(10)->create();
-        $user = User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // $user = User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
         $user->syncRoles(['super_admin']);
     }
 }
